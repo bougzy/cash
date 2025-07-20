0a3563ba6049376902deaaa1f6fa3f4101519895
bougzy <yubeespecial@gmail.com>
2025-07-20 18:10:02 +0100

    passion

--- js/main.js
+++ js/main.js
@@ -13,13 +13,13 @@
 
 
     // Fixed Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.fixed-top .container').addClass('shadow-sm').css('max-width', '100%');
-        } else {
-            $('.fixed-top .container').removeClass('shadow-sm').css('max-width', '85%');
-        }
-    });
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 300) {
+    //         $('.fixed-top .container').addClass('shadow-sm').css('max-width', '100%');
+    //     } else {
+    //         $('.fixed-top .container').removeClass('shadow-sm').css('max-width', '85%');
+    //     }
+    // });
 
 
     // Donation
